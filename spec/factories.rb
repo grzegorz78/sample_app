3c99cc58ee280401f8950837bca0b736966328a5
Grzegorz Such <grzesieks78@gmail.com>
2012-06-21 18:16:53 -0700

    Finish user signup - Chp 7

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "grzegorz such"
+    email    "grzesieks78@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,7 +22,7 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-  #it { should respond_to(:password_digest) }
+  it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
@@ -53,7 +53,7 @@ describe User do
       it { should_not be_valid }
     end
   end
-  
+    
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -61,6 +61,11 @@ describe User do
   
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+  
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
   
@@ -79,4 +84,13 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end  
+  
+  let(:found_user) { User.find_by_email(@user.email) }
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+    it { should_not == user_for_invalid_password }
+    specify { user_for_invalid_password.should be_false }
+  end
+
 end
